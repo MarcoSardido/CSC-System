9e2bc2d0b6f23c23f5b3fc3c9386d88a56509cb7
MarcoSardido <sardidomarco.student@gmail.com>
2021-09-01 16:47:41 +0800

    Initial Setup

---
+++ server.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const publicPage = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.use('/', publicPage.routes);
+
+app.listen(process.env.PORT || 3000, () => console.log('Server up and running...'));
